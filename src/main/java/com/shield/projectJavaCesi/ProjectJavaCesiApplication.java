7814ff72f5563e060134869b9d1dfa6e8b1eeef0
Sébastien XAVIER CARLOS <sebastien.xaviercarlos@hotmail.com>
2021-03-12 22:21:47 +0100

    first commit

---
+++ src/main/java/com/shield/projectJavaCesi/ProjectJavaCesiApplication.java
@@ -0,0 +1,13 @@
+package com.shield.projectJavaCesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectJavaCesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectJavaCesiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shield/projectJavaCesi/ProjectJavaCesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shield.projectJavaCesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectJavaCesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
