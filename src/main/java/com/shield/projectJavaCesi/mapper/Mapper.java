1b0cd823ad667b726eddec41cea5f34b83b81ef6
Dev <Dev@PC-Marie>
2021-03-19 20:04:31 +0100

    Create mapper functions for Civil and Organisation

--- src/main/java/com/shield/projectJavaCesi/mapper/Mapper.java
+++ src/main/java/com/shield/projectJavaCesi/mapper/Mapper.java
@@ -3,6 +3,9 @@ package com.shield.projectJavaCesi.mapper;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+
+import com.shield.projectJavaCesi.entity.being.Civil;
+import com.shield.projectJavaCesi.entity.being.Organisation;
 import com.shield.projectJavaCesi.entity.employee.Employee;
 import com.shield.projectJavaCesi.entity.employee.EmployeeDepartment;
 import com.shield.projectJavaCesi.entity.event.EventType;
@@ -11,6 +14,8 @@ import com.shield.projectJavaCesi.entity.superbeing.Superbeing;
 import com.shield.projectJavaCesi.resource.event.EventTypeResource;
 import com.shield.projectJavaCesi.resource.event.IncidentResource;
 import com.shield.projectJavaCesi.resource.SuperbeingResource;
+import com.shield.projectJavaCesi.resource.being.CivilResource;
+import com.shield.projectJavaCesi.resource.being.OrganisationResource;
 import com.shield.projectJavaCesi.resource.employee.EmployeeDepartmentResource;
 import com.shield.projectJavaCesi.resource.employee.EmployeeResource;
 
@@ -64,6 +69,60 @@ public class Mapper {
 		res.treasonRisk = superbeing.getTreasonRisk();
 		res.affiliated = superbeing.isAffiliated();
 		res.archive = superbeing.isArchive();
+		return res;
+	};
+	
+	public static Function<Civil, CivilResource> CivilToCivilResource = (civil) -> {
+		CivilResource res = new CivilResource();
+		res.id = civil.getId();
+		res.firstName = civil.getFirstName();
+		res.lastName = civil.getLastName();
+		res.socialSecurityNumber = civil.getSocialSecurityNumber();
+		res.gender = civil.getGender();
+		res.ref = civil.getRef();
+		res.organisation = civil.isOrganisation();
+		res.malevolant = civil.isMalevolant();
+		res.email = civil.getEmail();
+		res.password = civil.getPassword();
+		res.birthdate = civil.getBirthdate();
+		res.deathdate = civil.getDeathdate();
+		res.address = civil.getAddress();
+		res.city = civil.getCity();
+		res.state = civil.getState();
+		res.zipcode = civil.getZipcode();
+		res.coutry = civil.getCoutry();
+		res.mobilePhone = civil.getMobilePhone();
+		res.addedAt = civil.getAddedAt();
+		res.updateAt = civil.getUpdateAt();
+		res.howManyDeclaredIncident = civil.getHowManyDeclaredIncident();
+		res.victimOfHowManyMission = civil.getVictimOfHowManyMission();
+		res.archive = civil.isArchive();
+		return res;
+	};
+	
+	public static Function<Organisation, OrganisationResource> OrganisationToOrganisationResource = (organisation) -> {
+		OrganisationResource res = new OrganisationResource();
+		res.id = organisation.getId();
+		res.name = organisation.getName();
+		res.siret = organisation.getSiret();
+		res.ref = organisation.getRef();
+		res.organisation = organisation.isOrganisation();
+		res.malevolant = organisation.isMalevolant();
+		res.email = organisation.getEmail();
+		res.password = organisation.getPassword();
+		res.birthdate = organisation.getBirthdate();
+		res.deathdate = organisation.getDeathdate();
+		res.address = organisation.getAddress();
+		res.city = organisation.getCity();
+		res.state = organisation.getState();
+		res.zipcode = organisation.getZipcode();
+		res.coutry = organisation.getCoutry();
+		res.mobilePhone = organisation.getMobilePhone();
+		res.addedAt = organisation.getAddedAt();
+		res.updateAt = organisation.getUpdateAt();
+		res.howManyDeclaredIncident = organisation.getHowManyDeclaredIncident();
+		res.victimOfHowManyMission = organisation.getVictimOfHowManyMission();
+		res.archive = organisation.isArchive();
 		return res;
 	};
 
