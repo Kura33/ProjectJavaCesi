8a99a7b0d48c66cf8558000f3a86e1b59345fcee
Sébastien XAVIER CARLOS <sebastien.xaviercarlos@hotmail.com>
2021-03-21 11:56:07 +0100

    push Civil class

--- src/main/java/com/shield/projectJavaCesi/entity/being/Civil.java
+++ src/main/java/com/shield/projectJavaCesi/entity/being/Civil.java
@@ -2,77 +2,106 @@ package com.shield.projectJavaCesi.entity.being;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-
+import javax.persistence.ManyToOne;
 
 @Entity
 @DiscriminatorValue("0")
 public class Civil extends Being {
 
-	private String firstname;
-	private String lastname;
-	private String socialSecurityNumber;
-	private String gender;
-	
-	public static final String MR = "M";
-	public static final String MME = "Mme";
+    private String firstname;
+    private String lastname;
+    private String socialSecurityNumber;
+    private String title;
+    private String gender;
 
-	public enum Gender {
-		MR, MME;
-	}
-	
-	@ManyToMany
-	@JoinTable(name = "civil_on_organisation", joinColumns = @JoinColumn(name = "organisation_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "civil_id", referencedColumnName = "id"))
-	private List<Organisation> organisations = new ArrayList<>();
-	
-	public void addOrganisation(Organisation organisation) {
-		this.organisations.add(organisation);
-	}
+    public static final String TITLE_MR = "Mr";
+    public static final String TITLE_MME = "Mme";
 
-	public String getFirstname() {
-		return firstname;
-	}
+    public enum Title {
+        TITLE_MR, TITLE_MME
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstname = firstName;
-	}
+    public static final String GENDER_MR = "M";
+    public static final String GENDER_MME = "Mme";
 
-	public String getLastName() {
-		return lastname;
-	}
+    public enum Gender {
+        GENDER_MR, GENDER_MME
+    }
 
-	public void setLastName(String lastname) {
-		this.lastname = lastname;
-	}
+    @ManyToOne
+    @JoinColumn(name = "access_role_id", referencedColumnName = "id")
+    private AccessRole accessRole;
 
-	public String getSocialSecurityNumber() {
-		return socialSecurityNumber;
-	}
+    @ManyToMany
+    @JoinTable(name = "civil_on_organisation", joinColumns = @JoinColumn(name = "organisation_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "civil_id", referencedColumnName = "id"))
+    private List<Organisation> organisations = new ArrayList<>();
 
-	public void setSocialSecurityNumber(String socialSecurityNumber) {
-		this.socialSecurityNumber = socialSecurityNumber;
-	}
+    public void addOrganisation(Organisation organisation) {
+        this.organisations.add(organisation);
+    }
 
-	public String getGender() {
-		return gender;
-	}
+    public String getFirstname() {
+        return firstname;
+    }
 
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
 
-	public List<Organisation> getOrganisation() {
-		return organisations;
-	}
+    public String getLastname() {
+        return lastname;
+    }
 
-	public void setOrganisation(List<Organisation> organisation) {
-		this.organisations = organisations;
-	}
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public List<Organisation> getOrganisation() {
+        return organisations;
+    }
+
+    public AccessRole getAccessRole() {
+        return accessRole;
+    }
+
+    public void setAccessRole(AccessRole accessRole) {
+        this.accessRole = accessRole;
+    }
+
+    public List<Organisation> getOrganisations() {
+        return organisations;
+    }
+
+    public void setOrganisations(List<Organisation> organisations) {
+        this.organisations = organisations;
+    }
 }
